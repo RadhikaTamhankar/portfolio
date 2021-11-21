6776cace9a6c66013fc83103c7cdc8b9ec0fb9ed
ankurm23 <mishraap88@gmail.com>
2021-11-22 00:13:03 +0530

    neww

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function show1(){
+    document.getElementById("div2").style.display="none";
+    document.getElementById("div1").style.display="block";
+}
+function show2(){
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div2").style.display="block";
+}
